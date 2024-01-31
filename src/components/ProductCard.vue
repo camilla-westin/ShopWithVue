c52b5ce16c4206ddb657f3fb7a648528f87dfe41
Camilla Westin <camilla@webstin.se>
2024-01-31 11:02:53 +0100

    Created product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <VCard hover>
+    <v-img
+      :src="product.imgurl"
+      :alt="product.productName"
+      height="340px"
+      width="100%"
+      cover
+    ></v-img>
+    <VCardTitle>{{ product.productName }} </VCardTitle>
+    <VCardText>${{ product.price }}</VCardText>
+  </VCard>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Products from "@/data/products.json";
+import ProductCard from "@/components/ProductCard.vue";
+
+const products = ref(Products);
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <VContainer>
+      <VRow>
+        <VCol>
+          <h1>Home</h1>
+        </VCol>
+      </VRow>
+      <VRow tag="ul">
+        <VCol
+          tag="li"
+          v-for="product in products"
+          :key="product.id"
+          class="v-col-4"
+        >
+          <ProductCard :product="product" />
+        </VCol>
+      </VRow>
+    </VContainer>
+  </main>
 </template>
