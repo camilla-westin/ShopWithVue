58d1d0037b862c717de14ad6540430ccf0358994
Camilla Westin <camilla@webstin.se>
2024-03-08 08:14:06 +0100

    Re-organized files

--- nuxtsanityapp/app/components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="bg-greyish w-full h-12 flex align-center justify-between">
-    <h1 class="font-semibold text-center">ShopWithVue</h1>
-  </header>
-</template>
--- nuxtsanityapp/studio/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import blockContent from './blockContent'
-import post from './post'
-import products from './products'
-
-export const schemaTypes = [products, post, blockContent]
